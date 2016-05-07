73310680cfe7e4964bb70e3a745603818021f819
Diego Figueroa <diego.fv94@gmail.com>
2016-05-07 11:00:49 -0400

    add like system, better layout

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -5,8 +5,29 @@ app.controller('MainCtrl', [
 	function ($scope) {
 		$scope.test = 'Hello world!';
 		$scope.photos = [
-			{title: 'Boat', url: '/images/boat.jpg', likes: 0},
-			{title: 'Plane', url: '/images/plane.jpg', likes: 0}
+			{
+				title: 'Boat',
+				url: '/images/boat.jpg',
+				likes: 0
+			},
+			{
+				title: 'Plane',
+				url: '/images/plane.jpg',
+				likes: 0
+			}
 		];
+		$scope.addPhoto = function () {
+			if(!$scope.title || !$scope.url === ''){ return; }
+			$scope.photos.push({
+				title: $scope.title,
+				url: $scope.url,
+				likes: 0
+			});
+			$scope.title = '';
+			$scope.url = '';
+		};
+		$scope.likePhoto = function(photo){
+			photo.likes += 1;
+		};
 	}
 ]);
