2b84a59073483627b182bdb1cc54b0e087cefaaa
Diego Figueroa <diego.fv94@gmail.com>
2016-05-07 11:11:57 -0400

    refactor photos to be read from angular factory - add bootstrap styling - remove static list inside controller

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,21 +1,17 @@
-var app = angular.module('postMe', []);
+var app = angular.module('app', []);
+
+app.factory('photos', [ function(){
+	var o = {
+		photos: []
+	};
+	return o;
+}]);
 
 app.controller('MainCtrl', [
 	'$scope',
-	function ($scope) {
-		$scope.test = 'Hello world!';
-		$scope.photos = [
-			{
-				title: 'Boat',
-				url: '/images/boat.jpg',
-				likes: 0
-			},
-			{
-				title: 'Plane',
-				url: '/images/plane.jpg',
-				likes: 0
-			}
-		];
+	'photos',
+	function ($scope, photos) {
+		$scope.photos = photos.photos;
 		$scope.addPhoto = function () {
 			if(!$scope.title || !$scope.url === ''){ return; }
 			$scope.photos.push({
